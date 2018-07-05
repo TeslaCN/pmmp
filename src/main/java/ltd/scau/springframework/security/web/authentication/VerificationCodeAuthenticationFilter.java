f5c6a8454af12c7797c4e26e3f90f6fa04fcf6da
TeslaCN <root496879118@me.com>
2018-07-05 23:14:03 +0800

    紧急修复启动问题

--- src/main/java/ltd/scau/springframework/security/web/authentication/VerificationCodeAuthenticationFilter.java
+++ src/main/java/ltd/scau/springframework/security/web/authentication/VerificationCodeAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package ltd.scau.springframework.security.web.authentication;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -12,10 +14,20 @@ import javax.servlet.http.HttpServletResponse;
  * @author Weijie Wu
  */
 public class VerificationCodeAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private static final Log logger = LogFactory.getLog(VerificationCodeAuthenticationFilter.class);
+
+    public VerificationCodeAuthenticationFilter() {
+        super();
+    }
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         String verificationCode = (String) request.getSession().getAttribute("VERIFICATION_CODE");
         String inputCode = request.getParameter("verify");
+
+        logger.info("SESSION: " + verificationCode + ", INPUT: " + inputCode);
+
         if (inputCode != null && !inputCode.trim().equals("") && verificationCode != null && !verificationCode.trim().equals("") && inputCode.equalsIgnoreCase(verificationCode)) {
             return super.attemptAuthentication(request, response);
         }
